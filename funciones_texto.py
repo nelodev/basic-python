6be2418888b80bb51fadc31e40949fb6c851a22d
Nelo Puchades Bresó <correo@nelo.dev>
2021-01-05 21:43:40 +0100

    Playing with text functions & utilities

---
+++ funciones_texto.py
@@ -0,0 +1,23 @@
+nombre = input("¿Cuál es tu nombre?: ")
+# 'facundo '
+
+#UPPER - Mayúsculas
+print(nombre.upper())
+
+#CAPITALIZE - Primera letra mayúscula
+print(nombre.capitalize())
+
+#STRIP - Minúsculas
+print(nombre.lower())
+
+#STRIP - Eliminar espacios en blanco
+print(nombre.strip())
+
+#REPLACE - Sustituir chars
+print(nombre.replace('o', 'a'))
+
+#CHAR - Acceder a chars
+print(nombre[0])
+
+#LEN - Longitud de string
+print(len(nombre))
---
+++ slice.py
@@ -0,0 +1,17 @@
+nombre = 'Manuel'
+
+print(nombre[0:3]) #'Man'
+
+print(nombre[:3]) #'Man'
+
+print(nombre[3:]) #'uel'
+
+print(nombre[1:4]) #'anu'
+
+print(nombre[1:7:2]) #'aul'
+
+print(nombre[::]) #'Manuel'
+
+print(nombre[1::3]) #'ae'
+
+print(nombre[::-1]) #'leunaM
